ab6596eab980e67a13aa225f40a6b39ae811ff32
TR Jordan <tr@tern.sh>
2026-02-07 08:12:38 -0500

    TODO

--- tests/js/spec/components/alertLink.spec.jsx
+++ tests/js/spec/components/alertLink.spec.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 
-import {mountWithTheme} from 'sentry-test/enzyme';
+import {render, screen} from '@testing-library/react';
+
+import {renderWithTheme} from 'sentry-test/reactTestingLibrary';
 
 import AlertLink from 'app/components/alertLink';
 import {IconMail} from 'app/icons';
 
 describe('AlertLink', function () {
   it('renders', function () {
-    const wrapper = mountWithTheme(
+    renderWithTheme(
       <AlertLink to="/settings/accounts/notifications">
         This is an external link button
       </AlertLink>
     );
-    expect(wrapper).toSnapshot();
+    expect(screen.getByRole('link')).toBeInTheDocument();
+    expect(screen.getByText('This is an external link button')).toBeInTheDocument();
   });
 
   it('renders with icon', function () {
-    const wrapper = mountWithTheme(
+    renderWithTheme(
       <AlertLink to="/settings/accounts/notifications" icon={<IconMail />}>
         This is an external link button
       </AlertLink>
     );
-    expect(wrapper).toSnapshot();
+    expect(screen.getByRole('link')).toBeInTheDocument();
+    expect(screen.getByText('This is an external link button')).toBeInTheDocument();
   });
 });
