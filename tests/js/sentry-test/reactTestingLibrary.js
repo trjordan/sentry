2f4877884862c5d63fd9bdb225da0579a0fefea1
TR Jordan <tr@tern.sh>
2026-02-07 08:39:26 -0500

    TODO

---
+++ tests/js/sentry-test/reactTestingLibrary.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {cache} from '@emotion/css'; // eslint-disable-line emotion/no-vanilla
+import {CacheProvider, ThemeProvider} from '@emotion/react';
+import {render} from '@testing-library/react';
+
+import {lightTheme} from 'app/utils/theme';
+
+const renderWithTheme = (ui, opts) => {
+  const WrappingThemeProvider = ({children}) => (
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={lightTheme}>{children}</ThemeProvider>
+    </CacheProvider>
+  );
+
+  return render(ui, {wrapper: WrappingThemeProvider, ...opts});
+};
+
+export {renderWithTheme};
